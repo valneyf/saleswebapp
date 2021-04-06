dc346ee0072fa0fb2f084cca8d1e8174cbba9fd0
Valney Filho <valneyf@yahoo.com>
2021-04-06 11:11:12 -0300

    Add Department Scaffolding CRUD

--- SalesWebApp/Startup.cs
+++ SalesWebApp/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApp.Data;
 
 namespace SalesWebApp {
     public class Startup {
@@ -28,6 +30,9 @@ namespace SalesWebApp {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<SalesWebAppContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
