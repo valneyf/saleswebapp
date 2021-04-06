23303f71a8b6c1ffa57b7ebb52706f2fc797268c
Valney Filho <valneyf@yahoo.com>
2021-04-06 11:45:23 -0300

    MySql adaption and first migration

--- SalesWebApp/Startup.cs
+++ SalesWebApp/Startup.cs
@@ -32,7 +32,8 @@ namespace SalesWebApp {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
     services.AddDbContext<SalesWebAppContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppContext")));
+            options.UseMySql(Configuration.GetConnectionString("SalesWebAppContext"), builder =>
+                builder.MigrationsAssembly("SalesWebApp")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
