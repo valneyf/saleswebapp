1c7ac8bc6d6f2226be4ec12f9579b5f8ce539e05
Valney Filho <valneyf@yahoo.com>
2021-04-08 10:22:09 -0300

    Add referential integrity for Department

--- SalesWebApp/Migrations/20210408131716_DepartmentForeignKey.Designer.cs
+++ SalesWebApp/Migrations/20210408131716_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebApp.Data;
 
 namespace SalesWebApp.Migrations
 {
     [DbContext(typeof(SalesWebAppContext))]
-    partial class SalesWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20210408131716_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
